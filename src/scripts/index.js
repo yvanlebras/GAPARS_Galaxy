03d1d047b6a50712dd1946da2e415363a0c31587
Remi Planel <rplanel@pasteur.fr>
2019-07-05 17:54:28 +0200

    Good Luck Yvan ! We believe in you :)

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,18 +1,19 @@
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css'
-import "./index.css"
+import '@fortawesome/fontawesome-free/css/all.css'
+import "../styles/index.css"
 // 'use strict';
 
 $("#male-btn").on("click", () => console.log("male"))
 $("#female-btn").on("click", () => console.log("female"))
-
+$("#eco-img").on("click", () => console.log("change image"))
 
 
 async function main() {
     console.log('Calling MMOS API...');
 
-    const api = require('./lib/mmos-sdk-js-slim')({
+    const api = require('../../lib/mmos-sdk-js-slim')({
         protocol: 'https', host: 'api.depo.mmos.blue', port: 443,
         version: 'v2', game: 'yvan-le-bras-mnhn-fr',
         apiKey: {

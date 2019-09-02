b55b1a3fa3291a083f7498379266fe55bd77d805
Helena Rasche <hxr@hx42.org>
2019-09-02 13:19:38 +0200

    sep into lib

--- app.py
+++ app.py
@@ -1,107 +1,15 @@
 from flask import Flask
-import hashlib
-import hmac
-import json
-import time
-import requests
 from flask import render_template
-import random
 from flask import send_from_directory
 from flask import request
-import os
+import mmos
 
 app = Flask(__name__)
-
-
-class MMOSAPI:
-    def __init__(self):
-        self.protocol = "https"
-        self.host = "api.depo.mmos.blue"
-        self.port = 443
-        self.version = "v2"
-        self.game = "yvan-le-bras-mnhn-fr"
-        self.apiKey = {
-            "key": os.environ["MMOS_API_KEY"],
-            "secret": os.environ["MMOS_API_SECRET"],
-        }
-
-        self.playerCode = "YVAN001"
-        self.projectCode = "spipoll-fly"
-        self.urlBase = "%s://%s" % (self.protocol, self.host)
-
-    def digest(self, key, data):
-        h = hmac.new(bytes(str(key).encode("utf-8")), digestmod=hashlib.sha256)
-        h.update(data.encode("utf-8"))
-        return h.hexdigest()
-
-    def build_headers(self, method, path, body={}):
-        CONTENT_SEPARATOR = "|"
-        SIGNING_ALGORITHM = "MMOS1-HMAC-SHA256"
-        nonce = random.randint(0, 1000000000)
-        timestamp = int(time.time())
-
-        timestamp = 1567416082496
-        nonce = 222773350332
-
-        contentParts = [
-            SIGNING_ALGORITHM,
-            self.apiKey["key"],
-            timestamp,
-            nonce,
-            method,
-            path,
-            json.dumps(body, separators=(",", ":")),
-        ]
-        content = CONTENT_SEPARATOR.join(map(str, contentParts))
-        print(content)
-
-        signingKey = self.digest(timestamp, self.apiKey["secret"])
-        signature = self.digest(signingKey, content)
-
-        return {
-            "Content-Type": "application/json",
-            "X-MMOS-Algorithm": SIGNING_ALGORITHM,
-            "X-MMOS-Credential": self.apiKey["key"],
-            "X-MMOS-Timestamp": str(timestamp),
-            "X-MMOS-Nonce": str(nonce),
-            "X-MMOS-Signature": signature,
-        }
-
-    def info(self):
-        path = "/"
-        body = {}
-        headers = self.build_headers("GET", path, body)
-        r = requests.get(self.urlBase + path, headers=headers, data=body)
-        return r.json()
-
-    def create_task(self):
-        path = "/games/%s/players/%s/tasks" % (self.game, self.playerCode)
-        body = {
-            "projects": [self.projectcode],
-            "player": {"accountcode": self.playercode},
-        }
-        headers = self.build_headers("POST", path, body)
-        r = requests.post(self.urlBase + path, headers=headers, json=body)
-        return r.json()
-
-    def classify(self, sex, task_created, task_id):
-        path = "/classifications"
-        body = {
-            "task": {"id": task_id, "result": {"gender": sex}},
-            "circumstances": {"t": time.time() - int(task_created)},
-            "player": {"accountcode": self.playercode},
-            "playergroup": "group1122",
-        }
-        headers = self.build_headers("POST", path, body)
-        r = requests.post(self.urlBase + path, headers=headers, json=body)
-        return r.json()
-
-
-m = MMOSAPI()
+m = mmos.Client()
 
 
 def template(name, **context):
-    return render_template(name, host=request.host, config=app.config, **context)
+    return render_template(name, config=app.config, **context)
 
 
 @app.route("/img/<path:path>")
@@ -122,4 +30,5 @@ def index():
     return template("guess.html", t=None)
 
 
-app.run()
+if __name__ == '__main__':
+    app.run()
---
+++ mmos.py
@@ -0,0 +1,96 @@
+import os
+import hmac
+import random
+import time
+import json
+import hashlib
+import requests
+
+
+class Client:
+    def __init__(self):
+        self.protocol = "https"
+        self.host = "api.depo.mmos.blue"
+        self.port = 443
+        self.version = "v2"
+        self.game = "yvan-le-bras-mnhn-fr"
+        self.apiKey = {
+            "key": os.environ["MMOS_API_KEY"],
+            "secret": os.environ["MMOS_API_SECRET"],
+        }
+
+        self.playerCode = "YVAN001"
+        self.projectCode = "spipoll-fly"
+        self.urlBase = "%s://%s" % (self.protocol, self.host)
+
+    def digest(self, key, data):
+        h = hmac.new(bytes(str(key).encode("utf-8")), digestmod=hashlib.sha256)
+        h.update(data.encode("utf-8"))
+        return h.hexdigest()
+
+    def build_headers(self, method, path, body={}):
+        CONTENT_SEPARATOR = "|"
+        SIGNING_ALGORITHM = "MMOS1-HMAC-SHA256"
+        nonce = random.randint(0, 1000000000)
+        timestamp = int(time.time() * 1000)
+
+        timestamp = 1567416082496
+        # nonce = 222773350332
+
+        contentParts = [
+            SIGNING_ALGORITHM,
+            self.apiKey["key"],
+            timestamp,
+            nonce,
+            method,
+            path,
+            json.dumps(body, separators=(",", ":")),
+        ]
+        content = CONTENT_SEPARATOR.join(map(str, contentParts))
+
+        signingKey = self.digest(timestamp, self.apiKey["secret"])
+        signature = self.digest(signingKey, content)
+
+        return {
+            "Content-Type": "application/json",
+            "X-MMOS-Algorithm": SIGNING_ALGORITHM,
+            "X-MMOS-Credential": self.apiKey["key"],
+            "X-MMOS-Timestamp": str(timestamp),
+            "X-MMOS-Nonce": str(nonce),
+            "X-MMOS-Signature": signature,
+        }
+
+    def info(self):
+        path = "/"
+        body = {}
+        headers = self.build_headers("GET", path, body)
+        r = requests.get(self.urlBase + path, headers=headers, data=body)
+        return r.json()
+
+    def create_task(self):
+        path = "/games/%s/players/%s/tasks" % (self.game, self.playerCode)
+        body = {
+            "projects": [self.projectCode],
+            "player": {"accountCode": self.playerCode},
+        }
+        headers = self.build_headers("POST", path, body)
+        r = requests.post(self.urlBase + path, headers=headers, json=body)
+        return r.json()
+
+    def classify(self, sex, task_created, task_id):
+        path = "/classifications"
+        body = {
+            "task": {"id": task_id, "result": {"gender": sex}},
+            "circumstances": {"t": time.time() - int(task_created)},
+            "player": {"accountcode": self.playercode},
+            "playergroup": "group1122",
+        }
+        headers = self.build_headers("POST", path, body)
+        r = requests.post(self.urlBase + path, headers=headers, json=body)
+        return r.json()
+
+
+if __name__ == "__main__":
+    m = Client()
+    print(m.info())
+    print(m.create_task())
